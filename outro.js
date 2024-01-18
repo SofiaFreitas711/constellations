463f50e06f520eb5ba9744935ff6fe402bac40a5
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2024-01-18 20:15:58 +0000

    constellations added Co-authored-by: linax14 <linax14@users.noreply.github.com>

--- outro.js
+++ outro.js
@@ -28,6 +28,43 @@ class Star {
 
     endShape()
     pop()
+
+    
+  }
+}
+
+class Star02 {
+  constructor(x,y) {
+    this.opacity = 255
+    this.color = color("red")
+    this.x = random(0, w)
+    this.y = random(0, h)
+    this.scale = random(0.5, 1)
+  }
+
+  draw() {
+
+    push()
+    fill(this.color)
+    drawingContext.shadowBlur = 30
+    drawingContext.shadowColor = color("white")
+    beginShape()
+    vertex(this.x + 10, this.y + 2)
+    vertex(this.x + 14, this.y + 8)
+    vertex(this.x + 20, this.y + 8)
+    vertex(this.x + 16, this.y + 14)
+    vertex(this.x + 20, this.y + 20)
+    vertex(this.x + 10, this.y + 16)
+    vertex(this.x + 0, this.y + 20)
+    vertex(this.x + 4, this.y + 14)
+    vertex(this.x + 0, this.y + 8)
+    vertex(this.x + 6, this.y + 8)
+    vertex(this.x + 10, this.y + 2)
+
+    endShape()
+    pop()
+
+    
   }
 }
 
@@ -182,6 +219,7 @@ let rightKneeX = 0
 let rightKneeY = 0
 let skeleton
 let stars = []
+let stars02 = []
 let planetR;
 let planetL;
 let galaxy
@@ -189,8 +227,13 @@ let galaxy02
 let length
 let sky 
 let stage = 0
+let constellations = [{LWX: 282, LWY: 521, LEX:334, LEY:462, LSX:439, LSY:410, RSX:569, RSY:422},
+                      {LWX: 270, LWY: 310, LEX: 334, LEY:462, LSX:439,LSY:410, RSX:569,RSY:422},
+                      {LWX: 350, LWY: 280, LEX: 280, LEY: 380, LSX:400, LSY:405, RSX:550, RSY:410, REX: 670, REY:450, RWX:850, RWY:440}]
 
 function setup() {
+  // translate(w,0)
+  // scale(-1,1)
   let canvas = createCanvas(1000,750);
   canvas.position(250,0)
   video = createCapture(VIDEO)
@@ -210,6 +253,7 @@ function setup() {
   }
 
   length = stars.length
+
   stars[length] = new Star
   stars[length].x = leftShoulderX
   stars[length].y = leftShoulderY
@@ -280,27 +324,27 @@ function gotPoses(poses) {
     let RKX = pose.keypoints[14].position.x
     let RKY = pose.keypoints[14].position.y
     
-
-    leftShoulderX = lerp(leftShoulderX, LSX,0.5)
-    leftShoulderY = lerp(leftShoulderY, LSY,0.5)
-    rightShoulderX = lerp(rightShoulderX,RSX, 0.5)
-    rightShoulderY = lerp(rightShoulderY, RSY, 0.5)
-    leftElbowX = lerp(leftElbowX, LEX, 0.5)
-    leftElbowY = lerp(leftElbowY, LEY, 0.5)
-    rightElbowX = lerp(rightElbowX, REX, 0.5)
-    rightElbowY = lerp(rightElbowY, REY, 0.5)
-    leftWristX = lerp(leftWristX, LWX,0.5)
-    leftWristY = lerp(leftWristY, LWY, 0.5)
-    rightWristX = lerp(rightWristX, RWX, 0.5)
-    rightWristY = lerp(rightWristY, RWY, 0.5)
-    leftHipX = lerp(leftHipX, LHX, 0.5)
-    leftHipY = lerp(leftHipY, LHY, 0.5)
-    rightHipX = lerp(rightHipX, RHX, 0.5)
-    rightHipY = lerp(rightHipY, RHY, 0.5)
-    leftKneeX = lerp(leftKneeX, LKX, 0.5)
-    leftKneeY = lerp(leftKneeY, LKY, 0.5)
-    rightKneeX = lerp(rightKneeX, RKX, 0.5)
-    rightKneeY = lerp(rightKneeY, RKY, 0.5)
+let value = 0.1
+    leftShoulderX = lerp(leftShoulderX, LSX, value)
+    leftShoulderY = lerp(leftShoulderY, LSY, value)
+    rightShoulderX = lerp(rightShoulderX,RSX, value)
+    rightShoulderY = lerp(rightShoulderY, RSY, value)
+    leftElbowX = lerp(leftElbowX, LEX, value)
+    leftElbowY = lerp(leftElbowY, LEY, value)
+    rightElbowX = lerp(rightElbowX, REX, value)
+    rightElbowY = lerp(rightElbowY, REY, value)
+    leftWristX = lerp(leftWristX, LWX,value)
+    leftWristY = lerp(leftWristY, LWY, value)
+    rightWristX = lerp(rightWristX, RWX, value)
+    rightWristY = lerp(rightWristY, RWY, value)
+    leftHipX = lerp(leftHipX, LHX, value)
+    leftHipY = lerp(leftHipY, LHY, value)
+    rightHipX = lerp(rightHipX, RHX, value)
+    rightHipY = lerp(rightHipY, RHY,value)
+    leftKneeX = lerp(leftKneeX, LKX, value)
+    leftKneeY = lerp(leftKneeY, LKY, value)
+    rightKneeX = lerp(rightKneeX, RKX, value)
+    rightKneeY = lerp(rightKneeY, RKY, value)
 
   }
 }
@@ -326,15 +370,23 @@ function draw() {
   rect(0,0,w,h)
   pop()
   drawSkeleton();
-  // drawConstellation()
+  
+  drawConstellation()
 
   
 
   for (let i = 0; i < stars.length; i++) {
     let star = stars[i]
+    
     star.draw()
   }
 
+  for (let i = 0; i < stars02.length; i++) {
+    let star = stars02[i]
+    
+    star.draw()
+  }
+  
   stars[length].x = leftShoulderX -8
   stars[length].y = leftShoulderY -8
   stars[length+1].x = rightShoulderX -8
@@ -355,8 +407,6 @@ function draw() {
   stars[length+8].y = leftKneeY-8
   stars[length+9].x = rightKneeX-8
   stars[length+9].y = rightKneeY-8
-  
-  
 
   fill("red")
 
@@ -377,8 +427,55 @@ function drawSkeleton() {
   
 }
 
-// let constellations = [{},{},{}]
-
-// function drawConstellation(){
-
-// }
+function drawConstellation(){
+  
+  if(stage == 0){
+    stars02[0] = new Star02
+    stars02[0].x = constellations[0].LWX
+    stars02[0].y = constellations[0].LWY
+    stars02[1] = new Star02
+    stars02[1].x = constellations[0].LEX
+    stars02[1].y = constellations[0].LEY
+    stars02[2] = new Star02
+    stars02[2].x = constellations[0].LSX
+    stars02[2].y = constellations[0].LSY
+    stars02[3] = new Star02
+    stars02[3].x = constellations[0].RSX
+    stars02[3].y = constellations[0].RSY
+    
+  }
+  // else if(stage == 1){
+    // stars02 = []
+  //   stars[length] = new Star
+  //   stars[length].x = constellations[1].LWX
+  //   stars[length].y = constellations[1].LWY
+  //   stars[length+1] = new Star
+  //   stars[length+1].x = constellations[1].LEX
+  //   stars[length+1].y = constellations[1].LEY
+  //   stars[length+2] = new Star
+  //   stars[length+2].x = constellations[1].LSX
+  //   stars[length+2].y = constellations[1].LSY
+  //   stars[length+3] = new Star
+  //   stars[length+3].x = constellations[1].RSX
+  //   stars[length+3].y = constellations[1].RSY
+  // }else{
+  //   stars[length] = new Star
+  //   stars[length].x = constellations[2].LWX
+  //   stars[length].y = constellations[2].LWY
+  //   stars[length+1] = new Star
+  //   stars[length+1].x = constellations[2].LEX
+  //   stars[length+1].y = constellations[2].LEY
+  //   stars[length+2] = new Star
+  //   stars[length+2].x = constellations[2].LSX
+  //   stars[length+2].y = constellations[2].LSY
+  //   stars[length+3] = new Star
+  //   stars[length+3].x = constellations[2].RSX
+  //   stars[length+3].y = constellations[2].RSY
+  //   stars[length+4] = new Star
+  //   stars[length+4].x = constellations[2].REX
+  //   stars[length+4].y = constellations[2].REY
+  //   stars[length+5] = new Star
+  //   stars[length+5].x = constellations[2].RWX
+  //   stars[length+5].y = constellations[2].RWY
+  // }
+}
